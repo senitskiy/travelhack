a61451ae1558fd149363fda15c9e870677d4d079
Karev Pavel <karev3@yandex.ru>
2021-03-27 22:34:14 +0300

    fix CORS

--- russpass_id/app/user/init.js
+++ russpass_id/app/user/init.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 function initUser (app) {
   app.get('/', renderWelcome)
   app.get('/profile', passport.authenticationMiddleware(), renderProfile)
-  app.post('/login', passport.authenticate('local', {
+  app.post('/auth', passport.authenticate('local', {
     successRedirect: '/profile',
     failureRedirect: '/'
   }))
