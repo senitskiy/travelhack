36afb1d8a7ad686b19ea4de8213458198a648c57
Karev Pavel <karev3@yandex.ru>
2021-03-28 10:34:21 +0300

    заменил редирект на /order в партнере

--- partner/src/pages/Callback.jsx
+++ partner/src/pages/Callback.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Redirect } from 'react-router-dom'
 import './callback.scss'
 
 const AUTH_URL = 'http://localhost:8080/auth'
 
 export const Callback = () => {
-    const sendData = new URLSearchParams(window.location.search).get('sendData')
+    const [isCode, setCode] = useState(false)
 
     const postCode = async () => {
         const code = new URLSearchParams(window.location.search).get('code')
@@ -19,20 +20,24 @@ export const Callback = () => {
         const body = await response.text()
         const parseBody = JSON.parse(body)
 
+        setCode(true)
         const { name } = parseBody.response.data
-
-        localStorage.setItem('userName', name)
 
         // window.close()
         window.location.replace('/order')
-
     }
 
-    useEffect(postCode)
+    useEffect(() => {
+        postCode()
+    }, [])
 
     return (
         <div>
-            {sendData ? <div>Данные сохранены</div> : <div>Лоадер</div>}
-        </div>
+            {isCode ?
+                <Redirect to="/order" />
+                :
+                <div>Лоадер</div>
+            }
+        </div >
     )
 }
